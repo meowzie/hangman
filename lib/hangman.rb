6c76cebe0244a4f44fe86e25f09072793399f5a5
meowzie <appenge2@gmail.com>
2022-01-12 22:58:41 +0200

    Created class Game and moved class Word into it; Created method 'play' in class Game, which executes the code required to begin a game; Created class Answer which stores the answer and updates the blanks if a correct guess is made

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,17 +1,62 @@
 # frozen_string_literal: true
 
-# Creates words using the dictionary
-class Word
+# Creates a new game
+class Game
   def initialize
-    @dictionary = File.readlines('5desk.txt').map(&:chomp)
-    @chosen = ''
+    @word = Word.new
   end
 
-  def choose
-    until @chosen.length >= 5 && @chosen.length <= 12
-      index = rand(0..@dictionary.length - 1)
-      @chosen = @dictionary[index]
+  def play
+    word = @word.choose
+    answer = Answer.new(word)
+    blanks = answer.show
+    puts word
+    puts blanks
+    puts answer.update(gets.chomp)
+  end
+
+  # Creates words using the dictionary
+  class Word
+    attr_accessor :chosen
+
+    def initialize
+      @dictionary = File.readlines('5desk.txt').map(&:chomp)
+      @chosen = ''
     end
-    @chosen
+
+    def choose
+      until @chosen.length >= 5 && @chosen.length <= 12
+        index = rand(0..@dictionary.length - 1)
+        @chosen = @dictionary[index]
+      end
+      @chosen
+    end
+  end
+
+  # Contains functionality to display the blanks/answer and to allow the player to make guesses
+  class Answer
+    attr_accessor :blanks
+
+    def initialize(answer)
+      @answer = answer.downcase
+      @length = answer.length
+    end
+
+    def show
+      @blanks = ''.rjust(@length, '_')
+    end
+
+    def update(guess)
+      return puts 'Invalid input. Please input one letter' unless guess.length == 1
+
+      guess.downcase!
+      corrects = @answer.each_char.with_index.map { |letter, index| index if letter == guess }
+      corrects.compact!
+
+      corrects.each { |index| @blanks[index] = guess }
+      @blanks
+    end
   end
 end
+
+Game.new.play
